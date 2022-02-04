e40f4708e9dd27639950e26d71c3b61458f49ec1
Alex Dzyuba <alexeydzyuba27@gmail.com>
2022-02-04 16:51:52 +0300

    c

--- main.js
+++ main.js
@@ -3,11 +3,28 @@ require('dotenv').config()
 const { payMessage, paidMessage } = require('./messages')
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {channelMode: true})
+
+bot.telegram.setMyCommands(
+  [
+    {
+      command: '/pay',
+      description: 'Отправить сообщение, что ты ожидаешь оплату за занятие',
+    },
+    {
+      command: '/paid',
+      description: '_'
+    }
+  ]
+)
+
 bot.command('pay', async (ctx) => {
   return await ctx.reply(payMessage)
 })
 bot.command('paid', async (ctx) => {
   return await ctx.reply(paidMessage)
+})
+bot.command('help', async (ctx) => {
+  return await ctx.reply(`Используй команду /pay, чтобы отправить сообщение, что ты ожидаешь получить оплату за следующее занятие`)
 })
 
 bot.launch()
